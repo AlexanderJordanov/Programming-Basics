d0b92f69b9d15e1ef3cc5d6f3f2b2cb992d89c3e
Alexander Jordanov <146542674+AlexanderJordanov@users.noreply.github.com>
2024-01-25 14:57:08 +0200

    Add files via upload

---
+++ NestedLoops/Exercise/CinemaTickets/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace CinemaTickets
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string movieName = Console.ReadLine();
+            int totalTicketsCount = 0;
+            int studentTicketsCount = 0;
+            int standardTicketsCount = 0;
+            int kidTicketsCount = 0;
+
+            while (movieName != "Finish")
+            {
+                int capacity = int.Parse(Console.ReadLine());
+                int soldTickets = 0;
+                string ticketType = Console.ReadLine();
+
+                while (ticketType != "End")
+                {
+                    if (ticketType == "student") { studentTicketsCount++; }
+                    else if (ticketType == "standard") { standardTicketsCount++; }
+                    else if (ticketType == "kid") { kidTicketsCount++; }
+                    soldTickets++;
+                    if (soldTickets == capacity) { break; }
+                    ticketType = Console.ReadLine();
+                }
+                totalTicketsCount += soldTickets;
+
+                double fillPercentage = 100.0 * soldTickets / capacity;
+                Console.WriteLine($"{movieName} - {fillPercentage:f2}% full.");
+                movieName = Console.ReadLine();
+            }
+            Console.WriteLine($"Total tickets: {totalTicketsCount}");
+            Console.WriteLine($"{100.0 * studentTicketsCount / totalTicketsCount:f2}% student tickets.");
+            Console.WriteLine($"{100.0 * standardTicketsCount / totalTicketsCount:f2}% standard tickets.");
+            Console.WriteLine($"{100.0 * kidTicketsCount / totalTicketsCount:f2}% kids tickets.");
+        }
+    }
+}
---
+++ NestedLoops/Exercise/TrainTheTrainers/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace TrainTheTrainers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int judges = int.Parse(Console.ReadLine());
+            string input = Console.ReadLine();
+            double absTotal = 0;
+           
+            int counter = 0;
+            while (input != "Finish")
+            {
+                double total = 0;
+                double average = 0;
+                for (int i = 0; i < judges; i++)
+                {
+                    double grade = double.Parse(Console.ReadLine());
+                    total += grade;
+                }
+                average = total / judges;
+                Console.WriteLine($"{input} - {average:f2}.");
+
+                counter++;
+                absTotal += total;
+                
+                input = Console.ReadLine();
+            }
+            double absTotalAverage = absTotal / (counter * judges);
+            Console.WriteLine($"Student's final assessment is {absTotalAverage:f2}.");
+        }
+    }
+}
