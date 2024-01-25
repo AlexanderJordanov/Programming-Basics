a5ce4efd4651e7dafb5ebaa466a1e0727471ec8c
Alexander Jordanov <146542674+AlexanderJordanov@users.noreply.github.com>
2024-01-25 14:59:58 +0200

    Add files via upload

---
+++ NestedLoops/MoreExercise/ChallengeТheWedding/Program.cs
@@ -0,0 +1,17 @@
+﻿int male = int.Parse(Console.ReadLine());
+int female  = int.Parse(Console.ReadLine());
+int tables = int.Parse(Console.ReadLine());
+int tablesCount = 0;
+
+for (int m = 1; m <= male; m++)
+{
+   for (int f = 1; f <= female; f++)
+    {
+        Console.Write($"({m} <-> {f}) ");
+        tablesCount++;
+        if (tablesCount == tables)
+        {
+            return;
+        }
+    }
+}
