b122f7e99a5a3c9206f690617636766a16af7f54
Alexander Jordanov <146542674+AlexanderJordanov@users.noreply.github.com>
2024-01-25 14:06:14 +0200

    Add files via upload

---
+++ FirstStepsInCoding/PetShop/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace PetShop
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int dogFood = int.Parse(Console.ReadLine());
+            int catFood = int.Parse(Console.ReadLine());
+            double cena = dogFood * 2.50 + catFood * 4;
+            Console.WriteLine(cena);
+            //Console.WriteLine($"{dogFood * 2.50 + catFood *4} lv.");
+        }
+    }
+}
---
+++ FirstStepsInCoding/YardGreening/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace YardGreening
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double area = double.Parse(Console.ReadLine());
+            double price = area * 7.61;
+            double finalPrice = price - price*0.18;
+            double discount = price - finalPrice;
+            Console.WriteLine($"The final price is: {finalPrice} lv.");
+            Console.WriteLine($"The discount is: {discount}lv.");
+
+        }
+    }
+}
