44142f01b5689e88db60e88767a1c3895a15c4cc
Alexander Jordanov <146542674+AlexanderJordanov@users.noreply.github.com>
2024-01-25 14:08:29 +0200

    Add files via upload

---
+++ FirstStepsInCoding/InchestoCentimeters/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace InchestoCentimeters
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int inches = int.Parse(Console.ReadLine());
+            double santimeters = inches * 2.54;
+            Console.WriteLine(santimeters);
+        }
+    }
+}
