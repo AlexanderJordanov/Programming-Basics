04c3f3845ea40c9aac22d444b3d8d48af8de4c75
Alexander Jordanov <146542674+AlexanderJordanov@users.noreply.github.com>
2024-01-25 14:16:42 +0200

    Add files via upload

---
+++ FirstStepsInCoding/MoreExercise/Fishland/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Fishland
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double mackerelPricePerKilo = double.Parse(Console.ReadLine());
+            double sprinklePricePerKilo = double.Parse(Console.ReadLine());
+            double bonitoKilo = double.Parse(Console.ReadLine());
+            double safridKilo = double.Parse(Console.ReadLine());
+            int musslesKilo = int.Parse(Console.ReadLine());
+
+            double bonitoTotalPrice = (mackerelPricePerKilo + mackerelPricePerKilo * 0.6) * bonitoKilo;
+            double safridTotalPrice = (sprinklePricePerKilo + sprinklePricePerKilo * 0.8) * safridKilo;
+            double musslesTotalPrice = musslesKilo * 7.5;
+
+            Console.WriteLine($"{musslesTotalPrice + safridTotalPrice + bonitoTotalPrice:f2}");
+
+        }
+    }
+}
---
+++ FirstStepsInCoding/MoreExercise/VegetableMarket/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace VegetableMarket
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double vegetablesPricePerKilo = double.Parse(Console.ReadLine());
+            double fruitsPricePerKilo = double.Parse(Console.ReadLine());
+
+            int vegetablesAmount = int.Parse(Console.ReadLine());
+            int fruitsAmount = int.Parse(Console.ReadLine());
+
+            double totalPriceInLeva = vegetablesPricePerKilo*vegetablesAmount + fruitsPricePerKilo*fruitsAmount;
+            Console.WriteLine($"{totalPriceInLeva / 1.94:f2}");
+        }
+    }
+}
