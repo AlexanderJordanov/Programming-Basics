38e1845218dc950d2f11fad59a689f165e072019
Alexander Jordanov <146542674+AlexanderJordanov@users.noreply.github.com>
2024-01-25 14:07:27 +0200

    Add files via upload

---
+++ FirstStepsInCoding/RectangleArea/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace RectangleArea
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int sideA = int.Parse(Console.ReadLine());
+            int sideB = int.Parse(Console.ReadLine());
+            Console.WriteLine(sideA * sideB);
+
+        }
+    }
+}
