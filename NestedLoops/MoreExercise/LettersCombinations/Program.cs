6c9df3c6ae17250948c742c4454a2af72996d413
Alexander Jordanov <146542674+AlexanderJordanov@users.noreply.github.com>
2024-01-25 14:58:41 +0200

    Add files via upload

---
+++ NestedLoops/MoreExercise/LettersCombinations/Program.cs
@@ -0,0 +1,30 @@
+﻿char first = char.Parse(Console.ReadLine());
+char second = char.Parse(Console.ReadLine());
+char skipped  = char.Parse(Console.ReadLine());
+string skiped = "";
+
+int counter  = 0;
+string combination = "";
+
+for (char i = first; i <= second; i++)
+{
+    for (char j = first; j <= second; j++)
+    {
+        for (char k = first; k <= second; k++)
+        {
+            string firstLetter = char.ToString(i);
+            string secondLetter = char.ToString(j);
+            string thirdLetter = char.ToString(k);
+
+            skiped = char.ToString(skipped);
+
+            if (firstLetter != skiped && secondLetter != skiped && thirdLetter != skiped)
+            {
+                counter++;
+                combination = firstLetter + secondLetter + thirdLetter + " ";
+                Console.Write(combination);
+            }
+        }
+    }
+}
+Console.Write(counter);
