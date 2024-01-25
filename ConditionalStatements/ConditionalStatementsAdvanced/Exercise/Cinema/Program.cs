1a404269968e6aff4611650e2fffbf767df12a2f
Alexander Jordanov <146542674+AlexanderJordanov@users.noreply.github.com>
2024-01-25 14:32:34 +0200

    Add files via upload

---
+++ ConditionalStatements/ConditionalStatementsAdvanced/Exercise/Cinema/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Cinema
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string projectType = Console.ReadLine();
+            int rows = int.Parse(Console.ReadLine());
+            int colums = int.Parse(Console.ReadLine());
+            double income = 0;
+            if (projectType == "Premiere") { income = rows * colums * 12; }
+            else if (projectType == "Normal") { income = rows * colums * 7.5; }
+            else if (projectType == "Discount") { income = rows * colums * 5; }
+            Console.WriteLine($"{income:f2} leva");
+        }
+    }
+}
---
+++ ConditionalStatements/ConditionalStatementsAdvanced/Exercise/NewHouse/Program.cs
@@ -0,0 +1,59 @@
+﻿using Microsoft.VisualBasic;
+using System;
+
+namespace NewHouse
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string flowerType = Console.ReadLine();
+            int flowerCount = int.Parse(Console.ReadLine());
+            int budget = int.Parse(Console.ReadLine());
+
+            double price = 0;
+
+            switch (flowerType)
+            {
+                case "Roses": price = 5; break;
+                case "Dahlias": price = 3.8; break;
+                case "Tulips": price = 2.8; break;
+                case "Narcissus": price = 3; break;
+                case "Gladiolus": price = 2.5; break;
+            }
+
+            double totalPrice = price * flowerCount;
+
+            if (flowerType == "Roses" && flowerCount > 80)
+            {
+                totalPrice = 0.9 * totalPrice;
+            }
+            else if (flowerType == "Dahlias" && flowerCount > 90)
+            {
+                totalPrice = 0.85 * totalPrice;
+            }
+            else if (flowerType == "Tulips" && flowerCount > 80)
+            {
+                totalPrice = 0.85 * totalPrice;
+            }
+            else if (flowerType == "Narcissus" && flowerCount < 120)
+            { 
+                totalPrice = totalPrice * 0.15 + totalPrice;
+            }
+            else if (flowerType == "Gladiolus" && flowerCount < 80)
+            {
+                totalPrice = totalPrice * 0.2 + totalPrice;
+            }
+
+            if (budget >= totalPrice)
+            {
+                Console.WriteLine($"Hey, you have a great garden with {flowerCount} {flowerType} and {budget - totalPrice:f2} leva left.");
+            }
+
+            else
+            {
+                Console.WriteLine($"Not enough money, you need {totalPrice - budget:f2} leva more.");
+            }
+        }
+    }
+}
