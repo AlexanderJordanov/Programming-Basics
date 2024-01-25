8d7ee2f90949be10666596a739170ea572254746
Alexander Jordanov <146542674+AlexanderJordanov@users.noreply.github.com>
2024-01-25 14:37:05 +0200

    Add files via upload

---
+++ ForLoop/SumNumbers/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace SumNumbers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int numberAmount = int.Parse(Console.ReadLine());
+            int sum = 0;
+            for (int i = 1; i <= numberAmount; i++) 
+            {
+                int number = int.Parse(Console.ReadLine());
+                sum += number;
+            }
+            Console.WriteLine(sum);
+        }
+    }
+}
