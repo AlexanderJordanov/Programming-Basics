326e5edd994692ea78d98e5e8be67e1a4568de8c
Alexander Jordanov <146542674+AlexanderJordanov@users.noreply.github.com>
2024-01-25 14:24:09 +0200

    Add files via upload

---
+++ ConditionalStatements/Exercise/Time+15Minutes/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace Time_15Minutes
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int hours = int.Parse(Console.ReadLine());
+            int minutes = int.Parse(Console.ReadLine());
+
+            int minutesAfter = minutes + 15;
+
+            if (minutesAfter >= 60)
+            {
+                hours = hours + 1;
+                minutesAfter = minutesAfter - 60;
+            }
+            if ( hours > 23) 
+            {
+                hours = 0;
+            }
+            if (minutesAfter< 10)
+            {
+                Console.WriteLine($"{hours}:0{minutesAfter}");
+            }
+            else
+            {
+                Console.WriteLine($"{hours}:{minutesAfter}");
+            }
+        }
+    }
+}
