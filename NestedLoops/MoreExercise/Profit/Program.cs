924b623513a0ae60b7457e99a8db6317a1078452
Alexander Jordanov <146542674+AlexanderJordanov@users.noreply.github.com>
2024-01-25 15:01:16 +0200

    Add files via upload

---
+++ NestedLoops/MoreExercise/Profit/Program.cs
@@ -0,0 +1,21 @@
+﻿int oneLevCoins = int.Parse(Console.ReadLine());
+int twoLevCoins  = int.Parse(Console.ReadLine());
+int fiveLevBills = int.Parse(Console.ReadLine());
+int sumToCollect = int.Parse(Console.ReadLine());
+int actualSum = 0;
+
+for (int i = 0; i <= oneLevCoins; i++)
+{
+    for (int j = 0; j <= twoLevCoins; j++)
+    {
+        for (int k = 0; k <= fiveLevBills; k++)
+        {
+            actualSum = i * 1 + j * 2 + k * 5;
+
+            if (actualSum == sumToCollect)
+            {
+                Console.WriteLine($"{i} * 1 lv. + {j} * 2 lv. + {k} * 5 lv. = {actualSum} lv.");
+            }
+        }
+    }
+}
---
+++ NestedLoops/MoreExercise/SecretDoor'sLock/Program.cs
@@ -0,0 +1,23 @@
+﻿int hundredsMax = int.Parse(Console.ReadLine());
+int tensMax = int.Parse(Console.ReadLine());
+int unitsMax = int.Parse(Console.ReadLine());
+
+for (int hundredsNumber = 2; hundredsNumber <= hundredsMax; hundredsNumber++)
+{
+    for (int tensNumber = 2; tensNumber <= tensMax; tensNumber++)
+    { 
+        for (int unitsNumber = 2; unitsNumber <= unitsMax; unitsNumber++)
+        {
+            if (hundredsNumber % 2 == 0)
+            {
+                if (tensNumber == 2 || tensNumber == 3 || tensNumber == 5 || tensNumber == 7)
+                {
+                    if (unitsNumber %  2 == 0)
+                    {
+                        Console.WriteLine($"{hundredsNumber} {tensNumber} {unitsNumber}");
+                    }
+                }
+            }
+        } 
+    }
+}
