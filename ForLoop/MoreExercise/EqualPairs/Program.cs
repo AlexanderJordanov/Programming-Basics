5c6b305950ca8f8b5db522b14240d26abcf979e0
Alexander Jordanov <146542674+AlexanderJordanov@users.noreply.github.com>
2024-01-25 14:45:27 +0200

    Add files via upload

---
+++ ForLoop/MoreExercise/EqualPairs/Program.cs
@@ -0,0 +1,27 @@
+﻿int minSum = int.MaxValue;
+int maxSum = int.MinValue;
+int maxDiff = 0;
+int pairs = int.Parse(Console.ReadLine());
+for (int i = 0; i < pairs; i++)
+{
+    int currentNumber1 = int.Parse(Console.ReadLine());
+    int currentNumber2 = int.Parse(Console.ReadLine());
+    int currentSum = currentNumber1 + currentNumber2;
+    if (currentSum > maxSum)
+    {
+        maxSum = currentSum;
+    }
+    if (currentSum < minSum)
+    {
+        minSum = currentSum;
+    }
+    maxDiff = maxSum - minSum;
+}
+if (minSum == maxSum)
+{
+    Console.WriteLine($"Yes, value={minSum}");
+}
+if (minSum != maxSum && pairs != 0)
+{
+    Console.WriteLine($"No, maxdiff={maxDiff}");
+}
---
+++ ForLoop/MoreExercise/FootballLeague/Program.cs
@@ -0,0 +1,35 @@
+﻿int stadiumCapacity = int.Parse(Console.ReadLine());
+int amountOfFans  = int.Parse(Console.ReadLine());  
+int sectorA = 0, sectorB = 0, sectorV = 0, sectorG = 0;
+for  (int i = 0; i < amountOfFans; i++)
+{
+    string sector = Console.ReadLine();
+
+    if (sector == "A")
+    {
+        sectorA++;
+    }
+    else if (sector == "B")
+    {
+        sectorB++;
+    }
+    else if (sector == "V")
+    {
+        sectorV++;
+    }
+    else if (sector == "G")
+    {
+        sectorG++;
+    }
+}
+double percentSectorA = 100.0 * sectorA / amountOfFans;
+double percentSectorB = 100.0 * sectorB / amountOfFans;
+double percentSectorV = 100.0 * sectorV / amountOfFans;
+double percentSectorG = 100.0 * sectorG / amountOfFans;
+double percentOfStadium = 100.0 * amountOfFans / stadiumCapacity;
+
+Console.WriteLine($"{percentSectorA:f2}%");
+Console.WriteLine($"{percentSectorB:f2}%");
+Console.WriteLine($"{percentSectorV:f2}%");
+Console.WriteLine($"{percentSectorG:f2}%");
+Console.WriteLine($"{percentOfStadium:f2}%");
