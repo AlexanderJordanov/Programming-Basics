d0937d1f9e7e6b36a9325d39e99aaaf10fe141af
Alexander Jordanov <146542674+AlexanderJordanov@users.noreply.github.com>
2024-01-25 14:22:01 +0200

    Add files via upload

---
+++ ConditionalStatements/AreaOfFigures/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Net;
+
+namespace AreaOfFigures
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string figure = Console.ReadLine();
+
+            if (figure == "square")
+            {
+                double side = double.Parse(Console.ReadLine());
+                double area = side * side;
+
+                Console.WriteLine($"{area:F3}");
+            }
+            else if (figure == "rectangle")
+            {
+                double sideA = double.Parse(Console.ReadLine());
+                double sideB = double.Parse(Console.ReadLine());
+                double area = (sideA * sideB);
+
+                Console.WriteLine($"{area:F3}");
+            }
+            else if (figure == "circle")
+            {
+                double radius = double.Parse(Console.ReadLine());
+                double area = Math.PI * Math.Pow(radius, 2);
+
+                Console.WriteLine($"{area:F3}");
+            }
+            else if (figure == "triangle")
+            {
+                double sideA = double.Parse(Console.ReadLine());
+                double heightA = double.Parse(Console.ReadLine());
+                double area = sideA * heightA / 2;
+
+                Console.WriteLine($"{area:F3}");
+            }
+        }
+    }
+}
---
+++ ConditionalStatements/SpeedInfo/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace SpeedInfo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double rate = double.Parse(Console.ReadLine());
+
+            if (rate <= 10) 
+            {
+                Console.WriteLine("slow");
+            }
+            else if (rate <= 50)
+            {
+                Console.WriteLine("average");
+            }
+            else if ( rate <= 150)
+            {
+                Console.WriteLine("fast");
+            }
+            else if (rate <= 1000)
+            {
+                 Console.WriteLine("ultra fast");
+            }
+            else
+            {
+                Console.WriteLine("extremely fast");
+            }
+
+        }
+    }
+}
