2437a512fea53692221664b8ff287c31bd6b201c
Alexander Jordanov <146542674+AlexanderJordanov@users.noreply.github.com>
2024-01-25 14:50:21 +0200

    Add files via upload

---
+++ WhileLoop/Sequence2k+1/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Transactions;
+
+namespace Sequence2k_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int number = int.Parse(Console.ReadLine());
+            int currentNumber = 1;
+            while (currentNumber <= number )
+            {
+                Console.WriteLine(currentNumber);
+                currentNumber = currentNumber * 2 + 1;
+            }
+        }
+    }
+}
