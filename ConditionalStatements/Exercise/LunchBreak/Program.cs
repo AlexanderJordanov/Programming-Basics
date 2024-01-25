17875ec93132eb37ae136516b4b9c22964dd307b
Alexander Jordanov <146542674+AlexanderJordanov@users.noreply.github.com>
2024-01-25 14:26:25 +0200

    Add files via upload

---
+++ ConditionalStatements/Exercise/LunchBreak/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace LunchBreak
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string seriesName = Console.ReadLine();
+            int episodeDuration = int.Parse(Console.ReadLine());
+            int lunchBreakDuration = int.Parse(Console.ReadLine());
+
+            double timeRemaining = lunchBreakDuration - (lunchBreakDuration * 0.125 + lunchBreakDuration * 0.25);   
+
+            if (timeRemaining >= episodeDuration) 
+            {
+                Console.WriteLine($"You have enough time to watch {seriesName} and left with {Math.Ceiling(timeRemaining - episodeDuration)} minutes free time.");
+            }
+            else
+            {
+                Console.WriteLine($"You don't have enough time to watch {seriesName}, you need {Math.Ceiling(episodeDuration - timeRemaining)} more minutes.");
+            }
+        }
+    }
+}
---
+++ ConditionalStatements/Exercise/Shopping/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace Shopping
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double budget = double.Parse(Console.ReadLine());
+            int numberVideoCard = int.Parse(Console.ReadLine());
+            int numberProcessor = int.Parse(Console.ReadLine());
+            int numberRam = int.Parse(Console.ReadLine());
+
+            double priceVideoCard = numberVideoCard * 250;
+            double priceProcessor = priceVideoCard * 0.35 * numberProcessor;
+            double priceRam = priceVideoCard * 0.1 * numberRam;
+            double totalPrice = priceVideoCard + priceProcessor + priceRam;
+
+            if (numberVideoCard > numberProcessor)
+            {
+                totalPrice = totalPrice * 0.85;
+            }
+            if (budget >= totalPrice)
+            {
+                Console.WriteLine($"You have {budget - totalPrice:f2} leva left!");
+            }
+            else
+            {
+                Console.WriteLine($"Not enough money! You need {totalPrice - budget:f2} leva more!");
+            }
+        }
+    }
+}
