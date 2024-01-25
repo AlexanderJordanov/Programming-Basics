5fad75355f74a20fba44c54b6f670a42c68eb91d
Alexander Jordanov <146542674+AlexanderJordanov@users.noreply.github.com>
2024-01-25 14:38:11 +0200

    Add files via upload

---
+++ ForLoop/LeftAndRightSum/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace LeftAndRightSum
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            int firstSum = 0;
+            int secondSum = 0;
+
+            for (int i = 0; i < n; i++) { int number = int.Parse(Console.ReadLine()); firstSum += number; }
+            for (int i = 0; i < n; i++) { int number = int.Parse(Console.ReadLine()); secondSum += number; }
+
+            if (firstSum == secondSum) { Console.WriteLine($"Yes, sum = {firstSum}"); }
+            else { Console.WriteLine($"No, diff = {Math.Abs(firstSum - secondSum)}"); }
+
+        }
+    }
+}
---
+++ ForLoop/NumberSequence/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace NumberSequence
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int numberAmount = int.Parse(Console.ReadLine());
+
+            int max = int.MinValue;
+            int min = int.MaxValue;
+
+            for (int i = 0; i < numberAmount; i++)
+            {
+                int number = int.Parse(Console.ReadLine());
+
+                if (number < min) { min = number; }
+                if (number > max) { max = number; }
+            }
+            Console.WriteLine($"Max number: {max}");
+            Console.WriteLine($"Min number: {min}");
+        }
+    }
+}
