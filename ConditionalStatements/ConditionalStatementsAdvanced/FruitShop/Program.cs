876c7ee60f00f891ec4f4f83b75c494ab5f6900e
Alexander Jordanov <146542674+AlexanderJordanov@users.noreply.github.com>
2024-01-25 14:31:14 +0200

    Add files via upload

---
+++ ConditionalStatements/ConditionalStatementsAdvanced/FruitShop/Program.cs
@@ -0,0 +1,76 @@
+﻿using System;
+
+namespace FruitShop
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string fruit = Console.ReadLine();
+            string day = Console.ReadLine();
+            double quantity = double.Parse(Console.ReadLine());
+
+            double price = 0;
+
+            if (day == "Monday" || day == "Tuesday" || day == "Wednesday" || day == "Thursday" || day == "Friday")
+            {
+
+                switch (fruit)
+                {
+                    case "banana":
+                        price = quantity * 2.5; break;
+                    case "apple":
+                        price = quantity * 1.2; break;
+                    case "orange":
+                        price = quantity * 0.85; break;
+                    case "grapefruit":
+                        price = quantity * 1.45; break;
+                    case "kiwi":
+                        price = quantity * 2.7; break;
+                    case "pineapple":
+                        price = quantity * 5.5; break;
+                    case "grapes":
+                        price = quantity * 3.85; break;
+
+                    default: Console.WriteLine("error"); break;
+
+
+                }
+
+            }
+
+            else if (day == "Saturday" || day == "Sunday")
+            {
+                switch (fruit)
+                {
+                    case "banana":
+                        price = quantity * 2.7; break;
+                    case "apple":
+                        price = quantity * 1.25; break;
+                    case "orange":
+                        price = quantity * 0.9; break;
+                    case "grapefruit":
+                        price = quantity * 1.6; break;
+                    case "kiwi":
+                        price = quantity * 3; break;
+                    case "pineapple":
+                        price = quantity * 5.6; break;
+                    case "grapes":
+                        price = quantity * 4.2; break;
+                    default: Console.WriteLine("error"); break;
+                }
+
+            }
+            else
+            {
+                Console.WriteLine("error");
+            }
+
+            if (price > 0) 
+            {
+                Console.WriteLine($"{price:f2}");
+            }
+            
+        }
+    }
+}
---
+++ ConditionalStatements/ConditionalStatementsAdvanced/TradeCommissions/Program.cs
@@ -0,0 +1,69 @@
+﻿using System;
+
+namespace TradeCommissions
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Град	   0 ≤ s ≤ 500  /	500 < s ≤ 1 000  /	1 000 < s ≤ 10 000	/ s > 10 000
+            //Sofia     5 %                7 %                    8 %             12 %
+            //Varna     4.5 %              7.5 %                  10 %            13 %
+            //Plovdiv   5.5 %              8 %                    12 %            14.5 %
+
+
+                        string city = Console.ReadLine();
+            double sales = double.Parse(Console.ReadLine());
+
+            double commission = 0;
+
+           if ( sales >= 0 && sales <= 500)
+            { 
+                switch (city)
+                {
+                    case "Sofia": commission = sales * 0.05; break;
+                    case "Varna": commission = sales * 0.045; break;
+                    case "Plovdiv": commission = sales *0.055; break;
+                    default: Console.WriteLine("error"); break;
+                }
+                    
+
+            }
+           else if ( sales > 500 && sales <= 1000)
+            {
+                switch (city)
+                {
+                    case "Sofia": commission = sales * 0.07; break;
+                    case "Varna": commission = sales * 0.075; break;
+                    case "Plovdiv": commission = sales *0.08; break;
+                    default: Console.WriteLine("error"); break;
+                }
+            }
+           else if ( sales > 1000 && sales <= 10000)
+            {
+                switch (city)
+                {
+                    case "Sofia": commission = sales * 0.08; break;
+                    case "Varna": commission = sales * 0.1; break;
+                    case "Plovdiv": commission = sales * 0.12; break;
+                    default: Console.WriteLine("error"); break;
+                }
+            }
+           else if ( sales > 10000)
+            {
+                switch (city)
+                {
+                    case "Sofia": commission = sales * 0.12; break;
+                    case "Varna": commission = sales * 0.13; break;
+                    case "Plovdiv": commission = sales *0.145; break;
+                    default: Console.WriteLine("error"); break;
+                }
+            }
+           else
+            {
+                Console.WriteLine("error");
+            }
+           if ( commission > 0 ) { Console.WriteLine($"{commission:f2}"); }
+        }
+    }
+}
