2ff1d5dde8299d20c234dd7b41219e6618e06606
Alexander Jordanov <146542674+AlexanderJordanov@users.noreply.github.com>
2024-01-25 14:10:16 +0200

    Add files via upload

---
+++ FirstStepsInCoding/Exercise/DepositCalculator/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace DepositCalculator
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double sum = double.Parse(Console.ReadLine());
+            int term = int.Parse(Console.ReadLine());
+            double interest = double.Parse(Console.ReadLine());
+            Console.WriteLine(sum + term * (sum * interest * 0.01) / 12);
+        }
+    }
+}
