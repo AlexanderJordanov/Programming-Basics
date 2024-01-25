0ae915de6e82c1c52ffdb0f2482b739eab9f091b
Alexander Jordanov <146542674+AlexanderJordanov@users.noreply.github.com>
2024-01-25 14:35:30 +0200

    Add files via upload

---
+++ ConditionalStatements/ConditionalStatementsAdvanced/Exercise/HotelRoom/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+
+namespace HotelRoom
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string month = Console.ReadLine();
+            int days = int.Parse(Console.ReadLine());
+
+            double studioPrice = 0;
+            double apartamentPrice = 0;
+
+            if (month == "May" || month == "October")
+            {
+                studioPrice = 50 * days;
+                apartamentPrice = 65 * days;
+            }
+            else if (month == "June" || month == "September")
+            {
+                studioPrice = 75.20 * days;
+                apartamentPrice = 68.70 * days;
+            }
+            else if (month == "July" || month == "August")
+            {
+                studioPrice = 76 * days;
+                apartamentPrice = 77 * days;
+            }
+
+            if ((days > 7 && days <= 14) && (month == "May" || month == "October"))
+            {
+                studioPrice = 0.95 * studioPrice;
+            }
+
+            else if (days > 14)
+            {
+                apartamentPrice = 0.9 * apartamentPrice;
+
+                if (month == "May" || month == "October")
+                {
+                    studioPrice = 0.7 * studioPrice;
+                }
+                else if (month == "June" || month == "September")
+                {
+                    studioPrice = 0.8 * studioPrice;
+                }
+            }
+
+            Console.WriteLine($"Apartment: {apartamentPrice:f2} lv.");
+            Console.WriteLine($"Studio: {studioPrice:f2} lv.");
+        }
+    }
+}
---
+++ ConditionalStatements/ConditionalStatementsAdvanced/Exercise/SkiTrip/Program.cs
@@ -0,0 +1,68 @@
+﻿using System;
+
+namespace SkiTrip
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int days = int.Parse(Console.ReadLine());
+            string typeOfRoom = Console.ReadLine();
+            string grade = Console.ReadLine();
+
+            int nights = days - 1;
+            double visitPrice = 0;
+
+            if (days < 10)
+            {
+                if (typeOfRoom == "room for one person")
+                {
+                    visitPrice = 18 * nights;
+                }
+                else if (typeOfRoom == "apartment")
+                {
+                    visitPrice = (25 * nights) * 0.7;
+                }
+                else if (typeOfRoom == "president apartment")
+                {
+                    visitPrice = (35 * nights) * 0.9;
+                }
+            }
+            else if (days >= 10 && days <=15)
+            {
+                if (typeOfRoom == "room for one person")
+                {
+                    visitPrice = 18 * nights;
+                }
+                else if (typeOfRoom == "apartment")
+                {
+                    visitPrice = (25 * nights) * 0.65;
+                }
+                else if (typeOfRoom == "president apartment")
+                {
+                    visitPrice = (35 * nights) * 0.85;
+                }
+            }
+            else if (days > 15)
+            {
+                if (typeOfRoom == "room for one person")
+                {
+                    visitPrice = 18 * nights;
+                }
+                else if (typeOfRoom == "apartment")
+                {
+                    visitPrice = (25 * nights) * 0.5;
+                }
+                else if (typeOfRoom == "president apartment")
+                {
+                    visitPrice = (35 * nights) * 0.8;
+                }
+            }
+
+            if (grade == "positive") { visitPrice = visitPrice * 1.25; }
+            else { visitPrice = visitPrice * 0.9; }
+
+            Console.WriteLine($"{visitPrice:f2}");
+        }
+    }
+}
