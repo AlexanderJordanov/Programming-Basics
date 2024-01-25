82436568b30c38dc807e2a2d5c4ef0c7e997eb7b
Alexander Jordanov <146542674+AlexanderJordanov@users.noreply.github.com>
2024-01-25 14:52:56 +0200

    Add files via upload

---
+++ NestedLoops/Building/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace Building
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int floors = int.Parse(Console.ReadLine()); 
+            int rooms = int.Parse(Console.ReadLine());
+
+            for (int floor = floors; floor > 0; floor--)
+            {
+                for (int room = 0;  room < rooms; room++)
+                {
+                    if (floor == floors)
+                    {
+                        Console.Write($"L{floor}{room} ");
+                    }
+                    else if (floor % 2 == 1)
+                    {
+                        Console.Write($"A{floor}{room} ");
+                    }
+                    else
+                    {
+                        Console.Write($"O{floor}{room} ");
+                    }
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
---
+++ NestedLoops/SumOfTwoNumbers/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Reflection;
+
+namespace SumOfTwoNumbers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int start = int.Parse(Console.ReadLine());
+            int end = int.Parse(Console.ReadLine());
+            int magicNumber = int.Parse(Console.ReadLine());
+            int counter = 0;
+            bool found = false;
+            for (int i = start; i <= end; i++)
+            {
+                for (int j = start; j <= end; j++)
+                {
+                    int sum = i + j;
+                    counter++;
+                    if (sum == magicNumber) 
+                    {  
+                        Console.WriteLine($"Combination N:{counter} ({i} + {j} = {magicNumber})"); 
+                        found = true;
+                        break;
+                    }
+                }
+                if (found) { break; }
+            }
+            if (!found) { Console.WriteLine($"{counter} combinations - neither equals {magicNumber}"); }
+        }
+    }
+}
